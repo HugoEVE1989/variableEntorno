fc99e9d7899d22fc2e96e2276fe7c420f55630e0
197069 <hvargase@bancoazteca.com>
2020-04-15 13:22:44 -0500

    Carga inicial de proyecto

---
+++ src/main/java/com/heve/variable/entorno/ServiciOpenShiftApplication.java
@@ -0,0 +1,13 @@
+package com.heve.variable.entorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciOpenShiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heve/variable/entorno/ServiciOpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heve.variable.entorno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciOpenShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
